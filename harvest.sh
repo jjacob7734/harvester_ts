dc631b9ec0eb57ae229047659e1b729ce8978a9f
Joseph Jacob <Joseph.Jacob@jpl.nasa.gov>
2022-01-12 01:31:58 +0000

    Switch to using venv instead of conda.

--- harvest.sh
+++ harvest.sh
@@ -2,9 +2,12 @@
 #
 # Prerequisite: Set environment variable HARVESTER_HOME to point to the base 
 # directory containing the harvester directory and anaconda python.
-PY_EXEC=${HARVESTER_HOME}/anaconda3/bin/python
+PY_ENV=harvester
+#PY_EXEC=${HARVESTER_HOME}/anaconda3/bin/python
+PY_EXEC=${HARVESTER_HOME}/.venv/${PY_ENV}/bin/python
 PY_SCRIPT=${HARVESTER_HOME}/harvester/harvest.py
 export PYTHONPATH=${HARVESTER_HOME}:${PYTHONPATH}
 echo `date` Calling ${PY_EXEC} ${PY_SCRIPT} "$@"
+#source ${HARVESTER_HOME}/.venv/${PY_ENV}/bin/activate.csh
 exec ${PY_EXEC} ${PY_SCRIPT} "$@"
 echo `date` Finished ====================================
